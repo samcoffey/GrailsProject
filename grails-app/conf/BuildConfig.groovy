799910640a2edf2fee0392e44bda16a2da353ad0
Mark O'Looney <c11488678@mydit.ie>
2014-08-12 12:18:34 +0100

    test

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,6 +32,7 @@ grails.project.dependency.resolution = {
 		runtime 'com.javadocmd:simplelatlng:1.0.0'
 		runtime 'com.nimbusds:nimbus-jose-jwt:2.10.0'
 		runtime 'net.minidev:json-smart:1.0.9'
+		
     }
 
     plugins {
