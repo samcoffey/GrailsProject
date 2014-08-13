98da3bf5c59e9be4d7db24820ab39c732c02fdfe
samcoffey <samcoffey82@gmail.com>
2014-08-13 10:32:07 +0100

    Added postgresql as local db for dev and test environments

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,15 @@
 dataSource {
-	pooled = true
+	/*pooled = true
 	jmxExport = true
 	driverClassName = "org.h2.Driver"
 	username = "sa"
-	password = ""
+	password = ""*/
+	pooled = true
+	//dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+	url = "jdbc:postgresql://localhost:5432/grails"
+	driverClassName = "org.postgresql.Driver"
+	username = "grails"
+	password = "grails"
 }
 hibernate {
 	cache.use_second_level_cache = true
@@ -18,13 +24,15 @@ environments {
 	development {
 		dataSource {
 			dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-			url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			//url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			url = "jdbc:postgresql://localhost:5432/grails"
 		}
 	}
 	test {
 		dataSource {
 			dbCreate = "update"
-			url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			//url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			url = "jdbc:postgresql://localhost:5432/grails"
 		}
 	}
 	production {
